6dcc7087e37d7b82181d2222ae64bdab6a9dddd1
99choco99 <117999409+99choco99@users.noreply.github.com>
2025-12-08 15:09:40 +0900

    Add files via upload

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,7 @@ var request = require('request');
 const apiOptions = {
   server: 'http://localhost:3000'
 };
-if(process.env.NODE_ENV === 'developments'){
+if(process.env.NODE_ENV === 'production'){
   apiOptions.server = 'https://loc8r-api-indo.onrender.com'
 }
 
